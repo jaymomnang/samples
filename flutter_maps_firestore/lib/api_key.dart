889b01f4cd633de419eb16a01d1651b1b9fb765a
Julius Momnang <juliusmomnang@gmail.com>
2020-01-30 16:56:17 +0100

    google key

--- flutter_maps_firestore/lib/api_key.dart
+++ flutter_maps_firestore/lib/api_key.dart
@@ -2,6 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: Install a Google Maps API key that has Google Maps Web Services enabled
 // See https://developers.google.com/places/web-service/get-api-key
-const String googleMapsApiKey = 'ADD_A_KEY_HERE';
+const String googleMapsApiKey = 'AIzaSyCcuACoQDhiBTmZ-yjT4xjMms9b67ToMl8';
